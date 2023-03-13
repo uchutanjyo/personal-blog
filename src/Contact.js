ff295f805f811e37d962ece296b8341f2fa7a915
Matt Engerer <matt.engerer@aol.com>
2023-03-13 17:50:32 -0400

    fixed contact

--- src/Contact.js
+++ src/Contact.js
@@ -11,8 +11,7 @@ const Contact = () => {
     <div className="blogpost-container">
   
     <div className="about">
-   Give me a SHOUT at <p>matt.engerer@aol.com</p> or<p> 416 _________ .</p>
-          </div>
+  matt.engerer@gmail.com          </div>
       </div>
     </>
   );
